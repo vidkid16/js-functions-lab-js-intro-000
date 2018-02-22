bbefeaee2c2fae6c2cc3c74163edcfa1e78b0b64
Eliot Garcia <eag0828@gmail.com>
2018-02-22 04:19:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // write your code below!
+var happyHolidays =
 function happyHolidays() {
   return "Happy holidays!"
 }
+happyHolidays();
 
 function happyHolidaysTo("name") {
   return `Happy Holidays, ${name}!`
